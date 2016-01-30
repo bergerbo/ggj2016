3fe16dc936ef031304fc02753a985e32ed91b98a
zephyr <f.allard@student.isartdigital.com>
2016-01-30 08:19:20 +0100

    petit commit matinal

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManagerScript : MonoBehaviour {
+
+	public static GameManagerScript instance;
+
+	public static GameManagerScript GetInstance()
+	{
+		if(instance == null)
+		{
+			instance = this;
+		}
+		else
+		{
+			return instance;
+		}
+	}
+
+	public delegate void OnPause();
+	public event OnPause onPause;
+
+	void Awake(){
+		if(instance == null)
+		{
+			instance = this;
+		}
+		DontDestroyOnLoad(gameObject);
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
