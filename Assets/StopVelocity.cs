e2283bdeeb3c9aa0e234e63c8505d4dc7aa1af4c
Hoboris <berger.boris@orange.fr>
2016-01-31 12:11:55 +0100

    Fix slide

---
+++ Assets/StopVelocity.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StopVelocity : StateMachineBehaviour {
+
+    RigidbodyConstraints store;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log("Stop");
+        var rigidbody = animator.GetComponentInParent<Rigidbody>();
+        store = rigidbody.constraints;
+        rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        var rigidbody = animator.GetComponentInParent<Rigidbody>();
+        rigidbody.constraints = store;
+
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+}
